94c36b41c338849ae756ca35976f8c48793e35ac
Developer <crowdint@BlackLocus1.local>
2013-01-30 10:08:12 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+gem 'voting_app',
+    git: 'git://github.com/crowdint/voting_engine.git'
+
+group :production do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RequestsCrowdintCom::Application.config.secret_token = '9aef2ccee27bf8223197b07831cd47357286c480f4e36f4c5d324682065f8d8320ed4e7f7e19c67f85dffe86d5d63bf3a7318030af8fb997bbe32521e5c99e85'
