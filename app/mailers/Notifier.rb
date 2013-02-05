e501689ab69e2ddbe560e0d6941915771d654645
Developer <crowdint@BlackLocus1.local>
2013-02-05 10:10:41 -0600

    Avoid nils in the email list

--- app/mailers/Notifier.rb
+++ app/mailers/Notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   default from: 'requests@crowdint.com'
 
   def new_request
-    mail(subject: 'New request', to: User.all.map(&:email))
+    mail(subject: 'New request', to: User.pluck(:email).compact)
   end
 
 end
