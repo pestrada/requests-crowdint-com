9eb2ca80d0b8c4ec9066458b8587c72072c6a61d
Developer <crowdint@BlackLocus1.local>
2013-02-11 13:07:32 -0600

    Only send emails to the dev team (avoid spam)

--- app/mailers/Notifier.rb
+++ app/mailers/Notifier.rb
@@ -1,24 +1,25 @@
 class Notifier < ActionMailer::Base
-  default from: 'requests@crowdint.com'
+  default from: 'requests@crowdint.com',
+          to: %w(herman.moreno@crowdint.com ivan.calderon@crowdint.com gonzalo.fernandez@crowdint.com)
 
   def new_request
-    mail(subject: 'New request', to: User.pluck(:email).compact)
+    mail(subject: 'New request')
   end
 
   def request_promoted
-    mail(subject: 'Request promoted', to: User.pluck(:email).compact)
+    mail(subject: 'Request promoted')
   end
 
   def request_accepted
-    mail(subject: 'Request accepted', to: User.pluck(:email).compact)
+    mail(subject: 'Request accepted')
   end
 
   def request_completed
-    mail(subject: 'Request completed', to: User.pluck(:email).compact)
+    mail(subject: 'Request completed')
   end
 
   def request_rejected
-    mail(subject: 'Request rejected', to: User.pluck(:email).compact)
+    mail(subject: 'Request rejected')
   end
 
 end
