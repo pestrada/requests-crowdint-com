a829e465252f96cb1249e6489f48eae08acde44b
Developer <crowdint@BlackLocus2.local>
2013-03-13 09:47:04 -0600

    Don't send emails asynchronous

--- app/observers/submission_observer.rb
+++ app/observers/submission_observer.rb
@@ -5,17 +5,17 @@ class SubmissionObserver < ActiveRecord::Observer
 
   def notify_accepted(notification)
     @notification = Notifier.request_accepted(notification.submission)
-    AsyncJob.new(@notification).future(:deliver)
+    @notification.deliver
   end
 
   def notify_completed(notification)
     @notification = Notifier.request_completed(notification.submission)
-    AsyncJob.new(@notification).future(:deliver)
+    @notification.deliver
   end
 
   def notify_created(notification)
     @notification = Notifier.new_request(notification.submission)
-    AsyncJob.new(@notification).future(:deliver)
+    @notification.deliver
   end
 
   def notify_liked(notification)
@@ -23,11 +23,11 @@ class SubmissionObserver < ActiveRecord::Observer
 
   def notify_promoted(notification)
     @notification = Notifier.request_promoted(notification.submission)
-    AsyncJob.new(@notification).future(:deliver)
+    @notification.deliver
   end
 
   def notify_rejected(notification)
     @notification = Notifier.request_rejected(notification.submission)
-    AsyncJob.new(@notification).future(:deliver)
+    @notification.deliver
   end
 end
