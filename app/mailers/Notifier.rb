6b238ed4009739aaa5dca26501ed200ccb27645b
Ivan Calderon <iwano@hotmail.com>
2013-03-18 11:01:47 -0600

    Change notifications body

--- app/mailers/Notifier.rb
+++ app/mailers/Notifier.rb
@@ -1,32 +1,32 @@
 class Notifier < ActionMailer::Base
   include Celluloid
 
-  default from: 'requests@crowdint.com',
+  default from: 'Requests app <no-reply@crowdint.com>',
           to: ENV['EMAIL_LIST']
 
   def new_request(request)
     @request = request
-    mail(subject: 'New request')
+    mail(subject: "Request created by #{request.user.name}")
   end
 
   def request_promoted(request)
     @request = request
-    mail(subject: 'Request promoted')
+    mail(subject: "'#{request.user.name}''s request has been promoted")
   end
 
   def request_accepted(request)
     @request = request
-    mail(subject: 'Request accepted')
+    mail(subject: "'#{request.user.name}''s request has been accepted")
   end
 
   def request_completed(request)
     @request = request
-    mail(subject: 'Request completed')
+    mail(subject: "'#{request.user.name}''s request has been promoted")
   end
 
   def request_rejected(request)
     @request = request
-    mail(subject: 'Request rejected')
+    mail(subject: "'#{request.user.name}''s request has been rejected")
   end
 
 end
