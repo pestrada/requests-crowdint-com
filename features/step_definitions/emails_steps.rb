2d660b9691ab8f02f2de59a1ec25d60f3f4ac130
Ivan Calderon <iwano@hotmail.com>
2013-03-18 15:17:38 -0600

    Fix broken test

--- features/step_definitions/emails_steps.rb
+++ features/step_definitions/emails_steps.rb
@@ -2,5 +2,5 @@ Then /^I should receive a new request notification$/ do
   sleep 5
   email = ActionMailer::Base.deliveries.first
   email.should_not be_nil
-  email.subject.should eq 'New request'
+  email.subject.should include "Request created by"
 end
